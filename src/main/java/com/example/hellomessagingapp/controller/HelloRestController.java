703a68b192cb9bbe31f86e6efb37ad70bf1700bb
AashiiMittal <aashimittal2004@gmail.com>
2025-03-01 16:16:13 +0530

    hello with post

--- src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
+++ src/main/java/com/example/hellomessagingapp/controller/HelloRestController.java
@@ -1,10 +1,7 @@
 package com.example.hellomessagingapp.controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.hellomessagingapp.dto.UserDTO;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
@@ -22,5 +19,10 @@ public class HelloRestController {
     @GetMapping("/hello/param/{name}")
     public String sayHelloWithPathVariable(@PathVariable String name) {
         return "Hello " + name + " from BridgeLabz";
+    }
+
+    @PostMapping("/post")
+    public String sayHello(@RequestBody UserDTO user) {
+        return "Hello " + user.getFirstName() + " " + user.getLastName() + " from BridgeLabz";
     }
 }
